7de183145f947f4b2e7d79228ee5bc4e0ac0fd85
Lee Salminen <leesalminen@gmail.com>
2023-02-05 17:57:40 -0600

    better relay management

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,29 @@
 import React from 'react'
 import NostrChatWidget from './lib'
 
+const defaultRelayUrls = [
+  'wss://no.str.cr',
+  'wss://paid.no.str.cr',
+  'wss://nostr.fly.dev',
+  'wss://relay.snort.social',
+  'wss://relay.realsearch.cc',
+  'wss://relay.nostrgraph.net',
+  'wss://relay.minds.com/nostr/v1/ws',
+  'wss://nos.lol',
+  'wss://relay.current.fyi',
+  'wss://puravida.nostr.land',
+  'wss://nostr.milou.lol',
+  'wss://eden.nostr.land',
+  'wss://relay.damus.io',
+  'wss://nostr.oxtr.dev',
+]
+
+
 const App = () => {
   return (
     <NostrChatWidget
       recipientPk={'5f498ff809e02c5685e3bda193fcd7147a22e7b3971079549b0bb37643f3cacc'}
-      relayUrls={['wss://no.str.cr', 'wss://relay.damus.io', 'wss://nostr.fly.dev', 'wss://nostr.robotechy.com']} />
+      defaultRelayUrls={defaultRelayUrls} />
   )
 }
 
--- src/lib/components/NostrChatWidget.jsx
+++ src/lib/components/NostrChatWidget.jsx
@@ -1,14 +1,56 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { NostrProvider} from "nostr-react"
+import { relayInit } from "nostr-tools"
 
 import generateKeys from '../utils/index'
 
 import NostrWidgetContainer from './NostrWidgetContainer'
 
-const NostrChatWidget = ({recipientPk, relayUrls}) => {
+const NostrChatWidget = ({recipientPk, defaultRelayUrls}) => {
 	const [showWidget, setShowWidget] = useState(false)
 
 	const keys = generateKeys()
+	const [mostRecentKind3, setMostRecentKind3] = useState(0)
+	const [relayUrls, setRelayUrls] = useState(defaultRelayUrls)
+
+	useEffect(() => {
+	    async function getRelayList() {
+	      defaultRelayUrls.forEach(async (relayUrl) => {
+	        const relay = relayInit(relayUrl)
+	        await relay.connect()
+
+	        relay.on('connect', () => {
+	          const sub = relay.sub([
+	            {
+	              authors: [recipientPk],
+	              kinds: [3],
+	            }
+	          ])
+	          sub.on('event', event => {
+	            if(event.created_at > mostRecentKind3) {
+	              setMostRecentKind3(event.created_at)
+
+	              const relayList = JSON.parse(event.content)
+	              const relayUrls = Object.keys(relayList)
+
+	              if(relayUrls.length > 0) {
+	                const merged = new Set([...relayUrls, ...defaultRelayUrls])
+	                setRelayUrls([...merged])
+	              }
+	            }
+	          })
+	          sub.on('eose', () => {
+	            sub.unsub()
+	          })
+	        })
+	      })
+	    }
+
+	    if(recipientPk) {
+	      getRelayList()
+	    }
+	    
+	}, [recipientPk])
 
 	return (
 		<div className="NostrChatWidget">
